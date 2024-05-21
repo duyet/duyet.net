18ca8800b768b7807b60d2fec0840bbd72c7e8a8
Duyet Le <me@duyet.net>
2024-05-21 23:46:49 +0700

    chore: deno kv

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # duyet.net
 
-Simple redirect application built with Deno. It redirects various slugs to their corresponding URLs.
+Simple redirect application built with Deno. It redirects various slugs to their
+corresponding URLs.
 
-The application maps predefined slugs to specific URLs and performs HTTP 301 redirects.
+The application maps predefined slugs to specific URLs and performs HTTP 301
+redirects.
 
 ## Local Development
 
 To run the application locally, use the following command:
 
 ```bash
-deno run --allow-net --watch main.ts
+deno run --allow-net --unstable-kv --watch main.ts
 ```
 
 ## Deployment
--- main.ts
+++ main.ts
@@ -1,20 +1,47 @@
 function getUrl(slug: string): string {
   const urls: Record<string, string> = {
-    '/': 'https://blog.duyet.net',
-    '/blog': 'https://blog.duyet.net',
-    '/about': 'https://blog.duyet.net/about',
-    '/i': 'https://insights.duyet.net',
-    '/clickhouse': 'https://blog.duyet.net/tag/clickhouse',
-  }
+    "/": "https://blog.duyet.net",
+    "/blog": "https://blog.duyet.net",
+    "/about": "https://blog.duyet.net/about",
+    "/i": "https://insights.duyet.net",
+    "/clickhouse": "https://blog.duyet.net/tag/clickhouse",
+    "/tiktok": "https://www.tiktok.com/@duyet.net",
+    "/x": "https://x.com/_duyet",
+  };
 
-  return urls[slug] || urls['/']
+  return urls[slug] || urls["/"];
 }
 
-Deno.serve(async (req: Request) => {
-  const slug = '/' + (req.url.split('/').pop() || '')
-  const url = getUrl(slug)
+const kv = await Deno.openKv();
 
-  console.log(slug, '==> redirecting to', url)
+// Create a queue listener that will process enqueued messages
+kv.listenQueue(async (msg) => {
+  console.log(`[${msg.method}] [${msg.ip}]:`, ...msg.msg);
+});
 
-  return Response.redirect(url, 301)
-})
+const getLogger =
+  (req: Request, conn: Deno.ServeHandlerInfo) => async (...msg: string) => {
+    const method = req.method;
+    const ip = conn.remoteAddr.hostname;
+
+    const payload = {
+      method,
+      ip,
+      msg,
+    };
+
+    await kv.enqueue(payload);
+
+    console.log(...msg);
+  };
+
+Deno.serve(async (req: Request, conn: Deno.ServeHandlerInfo) => {
+  const logger = getLogger(req, conn);
+
+  const slug = "/" + (req.url.split("/").pop() || "");
+  const url = getUrl(slug);
+
+  await logger(slug, "==> redirecting to", url);
+
+  return Response.redirect(url, 301);
+});
