ec80fec06b8baa3964fba84560a45e92c1ce1408
Duyet Le <5009534+duyet@users.noreply.github.com>
2024-05-28 22:01:37 +0700

    feat: add /numi

--- urls.ts
+++ urls.ts
@@ -7,4 +7,5 @@ export const urls: Record<string, string> = {
   "/tiktok": "https://www.tiktok.com/@duyet.net",
   "/x": "https://x.com/_duyet",
   "/ni": "https://www.tiktok.com/@niniluungan",
+  "/numi": "https://numi.app",
 };
