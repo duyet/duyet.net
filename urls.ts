3aa746a2715a2c2713252f913cd9f95c4a2ae5ad
Duyet Le <me@duyet.net>
2024-06-01 14:42:14 +0700

    feat: add /ch

--- urls.ts
+++ urls.ts
@@ -4,6 +4,7 @@ export const urls: Record<string, string> = {
   "/about": "https://blog.duyet.net/about",
   "/i": "https://insights.duyet.net",
   "/clickhouse": "https://blog.duyet.net/tag/clickhouse",
+  "/ch": "https://blog.duyet.net/tag/clickhouse",
   "/tiktok": "https://www.tiktok.com/@duyet.net",
   "/x": "https://x.com/_duyet",
   "/ni": "https://www.tiktok.com/@niniluungan",
