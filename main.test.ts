f3e45c965bbe22c6e306582ba6c77ada09bcc93d
Duyet Le <me@duyet.net>
2024-07-21 15:37:28 +0700

    feat: add e2e test

---
+++ main.test.ts
@@ -0,0 +1,62 @@
+import { assertEquals } from "jsr:@std/assert";
+import { urls } from "./urls.ts";
+import { handler, initKv } from "./main.ts";
+import { assertGreaterOrEqual } from "jsr:@std/assert/assert-greater-or-equal";
+
+Deno.test("e2e", async (t) => {
+  const ac = new AbortController();
+  const kv = await initKv(":memory:");
+  const testHandler = handler(kv);
+
+  const server = Deno.serve({
+    async onListen(e) {
+      const baseUrl = new URL(`http://${e.hostname}:${e.port}`);
+
+      await t.step("Test /ping endpoint", async () => {
+        const res = await fetch(new URL("/ping", baseUrl));
+
+        assertEquals(res.status, 200);
+        assertEquals(await res.text(), "pong");
+      });
+
+      await t.step("Test /favicon.ico endpoint", async () => {
+        const res = await fetch(new URL("/favicon.ico", baseUrl));
+
+        assertEquals(res.status, 404);
+        res.body?.cancel();
+      });
+
+      await t.step("Test /_ls endpoint", async () => {
+        const res = await fetch(new URL("/_ls", baseUrl));
+
+        assertEquals(res.status, 200);
+        assertEquals(await res.json(), urls);
+      });
+
+      await t.step("Test redirect", async () => {
+        const slug = "/blog";
+        const expected = urls[slug];
+
+        const res = await fetch(new URL(slug, baseUrl), {
+          redirect: "manual",
+        });
+
+        assertEquals(res.status, 301);
+        assertGreaterOrEqual(res.headers.get("Location"), expected);
+        res.body?.cancel();
+      });
+
+      // Done
+      ac.abort();
+      kv.close();
+    },
+    signal: ac.signal,
+    handler: testHandler,
+    port: 3001,
+  });
+
+  await server.finished;
+
+  // "Gracefully close the server."
+  await server.shutdown();
+});
--- main.ts
+++ main.ts
@@ -2,48 +2,55 @@ import { UserAgent } from "./deps.ts";
 import { getClickHouse, getLogger, getSlug, getUrl } from "./libs.ts";
 import { urls } from "./urls.ts";
 
-const kv = await Deno.openKv();
+export const initKv = async (path?: string) => {
+  const kv = await Deno.openKv(path);
 
-// Create a queue listener that will process enqueued messages
-kv.listenQueue(async (msg) => {
-  console.log(`[${msg.method}] [${msg.ip}]:`, ...msg.msg);
+  // Create a queue listener that will process enqueued messages
+  kv.listenQueue(async (msg) => {
+    console.log(`[${msg.method}] [${msg.ip}]:`, ...msg.msg);
 
-  const ua = new UserAgent(msg.ua);
+    const ua = new UserAgent(msg.ua);
 
-  const source = msg.msg[0];
-  const target = msg.msg[2];
+    const source = msg.msg[0];
+    const target = msg.msg[2];
 
-  const clickhouse = getClickHouse();
-  const resp = await fetch(clickhouse.url, {
-    method: "POST",
-    headers: clickhouse.headers,
-    body:
-      `INSERT INTO duyet_analytics.duyet_redirect (source, target, ip, user_agent, browser, os_name, os_version, device_type)
-           VALUES ('${source}', '${target}', '${msg.ip}', '${msg.ua}', '${ua.browser.name}', '${ua.os.name}', '${ua.os.version}', '${ua.device.type}')`,
+    const clickhouse = getClickHouse();
+    const resp = await fetch(clickhouse.url, {
+      method: "POST",
+      headers: clickhouse.headers,
+      body:
+        `INSERT INTO duyet_analytics.duyet_redirect (source, target, ip, user_agent, browser, os_name, os_version, device_type)
+       VALUES ('${source}', '${target}', '${msg.ip}', '${msg.ua}', '${ua.browser.name}', '${ua.os.name}', '${ua.os.version}', '${ua.device.type}')`,
+    });
+
+    console.log("Clickhouse response:", await resp.text());
   });
 
-  console.log("Clickhouse response:", await resp.text());
-});
+  return kv;
+};
 
-Deno.serve(async (req: Request, conn: Deno.ServeHandlerInfo) => {
-  const logger = getLogger(req, conn, kv);
-  const slug = getSlug(req.url);
+export const handler =
+  (kv: Deno.Kv) => async (req: Request, conn: Deno.ServeHandlerInfo) => {
+    const logger = getLogger(req, conn, kv);
+    const slug = getSlug(req.url);
 
-  if (slug === "/ping" || slug === "/health") {
-    return new Response("pong");
-  }
+    if (slug === "/ping" || slug === "/health") {
+      return new Response("pong");
+    }
 
-  if (slug === "/favicon.ico") {
-    return new Response(null, { status: 404 });
-  }
+    if (slug === "/favicon.ico") {
+      return new Response(null, { status: 404 });
+    }
 
-  if (slug === "/_ls") {
-    return new Response(JSON.stringify(urls));
-  }
+    if (slug === "/_ls") {
+      return new Response(JSON.stringify(urls));
+    }
 
-  // Redirect to the target URL
-  const url = getUrl(slug);
-  await logger(req.url, "==> redirecting to", url);
+    // Redirect to the target URL
+    const url = getUrl(slug);
+    await logger(req.url, "==> redirecting to", url);
 
-  return Response.redirect(url, 301);
-});
+    return Response.redirect(url, 301);
+  };
+
+Deno.serve(handler(await initKv()));
