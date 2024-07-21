24d8c2e7f3a560923384c9ccde6f7ccd638a3453
Duyet Le <me@duyet.net>
2024-07-21 14:24:57 +0700

    fix: deno fmt

--- urls.ts
+++ urls.ts
@@ -4,12 +4,12 @@ export const urls: Record<string, string> = {
   "/about": "https://blog.duyet.net/about",
   "/i": "https://insights.duyet.net",
   "/clickhouse": "https://blog.duyet.net/tag/clickhouse",
-  "/ch": "https://blog.duyet.net/tag/clickhouse",
   "/tiktok": "https://www.tiktok.com/@duyet.net",
   "/tt": "https://www.tiktok.com/@duyet.net",
   "/x": "https://x.com/_duyet",
   "/ni": "https://www.tiktok.com/@niniluungan",
   "/numi": "https://numi.app",
-  "/ch": "https://blog.duyet.net/series/clickhouse-on-kubernetes?utm_source=duyet.net&utm_medium=linkedin&utm_campaign=duyet.net",
+  "/ch":
+    "https://blog.duyet.net/series/clickhouse-on-kubernetes?utm_source=duyet.net&utm_medium=linkedin&utm_campaign=duyet.net",
   "/mo": "https://monica.im/invitation?c=RJF8T7RT",
 };
