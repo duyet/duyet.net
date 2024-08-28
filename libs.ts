e5688eafb40be0540e320e100ac57cc1dae0eaa9
Duyet Le <me@duyet.net>
2024-08-28 14:22:31 +0700

    feat: add /_stats

--- libs.ts
+++ libs.ts
@@ -39,6 +39,26 @@ export const getClickHouse = (): {
   };
 };
 
+export const getStats = async () => {
+  const clickhouse = getClickHouse();
+
+  const resp = await fetch(clickhouse.url, {
+    method: "POST",
+    headers: clickhouse.headers,
+    body: `SELECT
+               source,
+               count()
+           FROM duyet_analytics.duyet_redirect
+           GROUP BY 1
+           ORDER BY 2 DESC
+           LIMIT 50
+           Format JSON`,
+  });
+
+  const text = await resp.text();
+  return JSON.parse(text);
+};
+
 export const getLogger =
   (req: Request, conn: Deno.ServeHandlerInfo, kv: Deno.Kv) =>
   async (...msg: string[]) => {
