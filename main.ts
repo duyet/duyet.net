46af5f83b1ab93fa9692008a0b32d1ee49da3ffd
Duyet Le <me@duyet.net>
2024-09-25 17:16:53 +0700

    chore: update logging

--- main.ts
+++ main.ts
@@ -7,10 +7,9 @@ export const initKv = async (path?: string) => {
 
   // Create a queue listener that will process enqueued messages
   kv.listenQueue(async (msg) => {
-    console.log(`[${msg.method}] [${msg.ip}]:`, ...msg.msg);
+    // console.log(`[${msg.method}] [${msg.ip}]:`, ...msg.msg);
 
     const ua = new UserAgent(msg.ua);
-
     const source = msg.msg[0];
     const target = msg.msg[2];
 
@@ -22,11 +21,13 @@ export const initKv = async (path?: string) => {
         headers: clickhouse.headers,
         body:
           `INSERT INTO duyet_analytics.duyet_redirect (source, target, ip, user_agent, browser, os_name, os_version, device_type)
-           VALUES ('${source}', '${target}', '${msg.ip}', '${msg.ua}', '${ua.browser.name}', '${ua.os.name}', '${ua.os.version}', '${ua.device.type}')`,
+           VALUES ('${source}', '${target}', '${msg.ip}', '${msg.ua}', '${ua.browser.name}', '${ua.os.name}', '${ua.os.version}', '${ua.device.type}')
+           Format JSON`,
       });
-      console.log("Inserted logs to ClickHouse", await resp.text());
+      const json = await resp.json();
+      console.log(`Inserted logs to ClickHouse: ${json}, msg:`, msg);
     } catch (e) {
-      console.error("Clickhouse error:", e);
+      console.error(`Clickhouse error: ${e}, msg:`, msg);
     }
   });
 
